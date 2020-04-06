57e6e78cfc6088da91fe33f2d105d52c2117a457
Shailesh Das <57151662+Angry-BOT@users.noreply.github.com>
2020-04-07 02:46:18 +0530

    Update Detection.py

--- Detection.py
+++ Detection.py
@@ -26,14 +26,14 @@ PATH_TO_LABELS = 'H:\Projects\AITrafficControlSystem\object_detection\data\mscoc
 
 NUM_CLASSES = 90
 
-# Download Model
-#opener = urllib.request.URLopener()
-#opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-#tar_file = tarfile.open(MODEL_FILE)
-#for file in tar_file.getmembers():
-#    file_name = os.path.basename(file.name)
-#    if 'frozen_inference_graph.pb' in file_name:
-#        tar_file.extract(file, os.getcwd())
+#Download Model
+opener = urllib.request.URLopener()
+opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+tar_file = tarfile.open(MODEL_FILE)
+for file in tar_file.getmembers():
+    file_name = os.path.basename(file.name)
+    if 'frozen_inference_graph.pb' in file_name:
+        tar_file.extract(file, os.getcwd())
 
 # Load a (frozen) Tensorflow model into memory.
 detection_graph = tf.Graph()
